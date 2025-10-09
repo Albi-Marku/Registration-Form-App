57f7a9150681786f8a66105cc43ac48ee5a35c3f
Albi Marku <albi.marku@ss.local>
2025-10-09 09:55:26 +0200

    Changes

--- obj/Debug/net8.0/RegistrationFormApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/RegistrationFormApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RegistrationFormApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad153ceb190a5d606a4c0adc7df031fc73f3399e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a3d6ff1ca360cd11e2cd865ad4d4b7a6a51d3a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("RegistrationFormApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RegistrationFormApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
